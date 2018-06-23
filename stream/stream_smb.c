5b395a0e73261b74b8bfd1aaf2dfc13089973a89
Yclept Nemo <pscjtwjdjtAhnbjm/dpn>
2018-06-22 23:03:31 -0400

    stream/stream_smb.c: fix #5936 by partial locking
    Thread-unsafe libsmbclient crashes when used across threads, for example
    when provided more than one "smb://" URI. This patch allows each library
    function to run concurrently with itself while locking out other library
    functions, in an attempt to boost the performance of the
    `smbclient_lock_all` branch. Unfortunately it fails horribly, so just
    here for posterity.

--- stream/stream_smb.c
+++ stream/stream_smb.c
@@ -21,6 +21,7 @@
 
 #include <libsmbclient.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "common/msg.h"
 #include "stream.h"
@@ -30,6 +31,53 @@
 #if !HAVE_GPL
 #error GPL only
 #endif
+
+static int count_lseek = 0;
+static int count_read = 0;
+static int count_write = 0;
+static int count_close = 0;
+static int count_open = 0;
+static int count_init = 0;
+static pthread_cond_t smb_cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t smb_lock = PTHREAD_MUTEX_INITIALIZER;
+
+// The 'condition_expr' could be the sum of all count variables minus
+// 'count_var' and therefore not required, but that is less efficient.
+#define GEN_LOCKED(condition_expr, count_var, function, result_type, ...)\
+    pthread_mutex_lock(&smb_lock);\
+    while(condition_expr) { pthread_cond_wait(&smb_cond, &smb_lock); }\
+    count_var++;\
+    pthread_mutex_unlock(&smb_lock);\
+    result_type result = function(__VA_ARGS__);\
+    pthread_mutex_lock(&smb_lock);\
+    count_var--;\
+    if (!count_var) { pthread_cond_broadcast(&smb_cond); }\
+    pthread_mutex_unlock(&smb_lock);\
+    return result;
+
+static off_t smbc_lseek_locked(int fd, off_t offset, int whence) {
+    GEN_LOCKED(count_read || count_write || count_close || count_open || count_init, count_lseek, smbc_lseek, off_t, fd, offset, whence)
+}
+
+static ssize_t smbc_read_locked(int fd, void *buf, size_t bufsize) {
+    GEN_LOCKED(count_lseek || count_write || count_close || count_open || count_init, count_read, smbc_read, ssize_t, fd, buf, bufsize)
+}
+
+static ssize_t smbc_write_locked(int fd, const void *buf, size_t bufsize) {
+    GEN_LOCKED(count_lseek || count_read || count_close || count_open || count_init, count_write, smbc_write, ssize_t, fd, buf, bufsize)
+}
+
+static int smbc_close_locked(int fd) {
+    GEN_LOCKED(count_lseek || count_read || count_write || count_open || count_init, count_close, smbc_close, int, fd)
+}
+
+static int smbc_open_locked(const char *furl, int flags, mode_t mode) {
+    GEN_LOCKED(count_lseek || count_read || count_write || count_close || count_init, count_open, smbc_open, int, furl, flags, mode)
+}
+
+static int smbc_init_locked(smbc_get_auth_data_fn fn, int debug) {
+    GEN_LOCKED(count_lseek || count_read || count_write || count_close || count_open, count_init, smbc_init, int, fn, debug)
+}
 
 struct priv {
     int fd;
@@ -46,8 +94,8 @@ static int control(stream_t *s, int cmd, void *arg) {
   struct priv *p = s->priv;
   switch(cmd) {
     case STREAM_CTRL_GET_SIZE: {
-      off_t size = smbc_lseek(p->fd,0,SEEK_END);
-      smbc_lseek(p->fd,s->pos,SEEK_SET);
+      off_t size = smbc_lseek_locked(p->fd,0,SEEK_END);
+      smbc_lseek_locked(p->fd,s->pos,SEEK_SET);
       if(size != (off_t)-1) {
         *(int64_t *)arg = size;
         return 1;
@@ -60,7 +108,7 @@ static int control(stream_t *s, int cmd, void *arg) {
 
 static int seek(stream_t *s,int64_t newpos) {
   struct priv *p = s->priv;
-  if(smbc_lseek(p->fd,newpos,SEEK_SET)<0) {
+  if(smbc_lseek_locked(p->fd,newpos,SEEK_SET)<0) {
     return 0;
   }
   return 1;
@@ -68,7 +116,7 @@ static int seek(stream_t *s,int64_t newpos) {
 
 static int fill_buffer(stream_t *s, char* buffer, int max_len){
   struct priv *p = s->priv;
-  int r = smbc_read(p->fd,buffer,max_len);
+  int r = smbc_read_locked(p->fd,buffer,max_len);
   return (r <= 0) ? -1 : r;
 }
 
@@ -77,7 +125,7 @@ static int write_buffer(stream_t *s, char* buffer, int len) {
   int r;
   int wr = 0;
   while (wr < len) {
-    r = smbc_write(p->fd,buffer,len);
+    r = smbc_write_locked(p->fd,buffer,len);
     if (r <= 0)
       return -1;
     wr += r;
@@ -88,7 +136,7 @@ static int write_buffer(stream_t *s, char* buffer, int len) {
 
 static void close_f(stream_t *s){
   struct priv *p = s->priv;
-  smbc_close(p->fd);
+  smbc_close_locked(p->fd);
 }
 
 static int open_f (stream_t *stream)
@@ -110,13 +158,13 @@ static int open_f (stream_t *stream)
     return STREAM_ERROR;
   }
 
-  err = smbc_init(smb_auth_fn, 1);
+  err = smbc_init_locked(smb_auth_fn, 1);
   if (err < 0) {
     MP_ERR(stream, "Cannot init the libsmbclient library: %d\n",err);
     return STREAM_ERROR;
   }
 
-  fd = smbc_open(filename, m,0644);
+  fd = smbc_open_locked(filename, m,0644);
   if (fd < 0) {
     MP_ERR(stream, "Could not open from LAN: '%s'\n", filename);
     return STREAM_ERROR;
@@ -124,8 +172,8 @@ static int open_f (stream_t *stream)
 
   len = 0;
   if(!write) {
-    len = smbc_lseek(fd,0,SEEK_END);
-    smbc_lseek (fd, 0, SEEK_SET);
+    len = smbc_lseek_locked(fd,0,SEEK_END);
+    smbc_lseek_locked (fd, 0, SEEK_SET);
   }
   if(len > 0 || write) {
     stream->seekable = true;
